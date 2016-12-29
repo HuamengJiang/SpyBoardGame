d065b44c537972cc8966aa5384b2d0f04f5b9e2b
Jeremy-Xin <xinchengjie@126.com>
2016-12-30 02:15:04 +0800

    game wrapper

--- app/Game.py
+++ app/Game.py
@@ -1,5 +1,27 @@
 class Game:
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.board = GameBoard("./data/TestText")
+        self.board.construct_word_set(True)
+        self.board.construct_main_board()
+        self.board.construct_tiling()
+        # for i in range(g.n):
+        #     print g.tiling[i]
+        self.teams = []
 
-	
+     def is_open(self):
+     	pass
+
+     def add_player(self):
+     	pass
+
+     def start_game(self):
+     	pass
+
+     def terminate_game(self):
+     	pass
+
+
+class Team:
+	self.color = ""
+	self.players = []
+	self.leader = None
